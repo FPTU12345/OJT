c7eb1097354b4ff1901d42f116090d9307ccdb77
FPTU12345 <114289940+FPTU12345@users.noreply.github.com>
2024-03-02 11:14:30 +0700

    Add files via upload

---
+++ deploy.py
@@ -0,0 +1,24 @@
+import os
+from pyngrok import ngrok
+from threading import Thread
+
+class Streamlit:
+  def __init__(self,
+               auth_token = '2ctTxXQENM9On1GMyfUCaeA9Dxg_83vzLhmYYRJ9267BRzSPZ',
+               app_path = 'app.py',
+               port = 8000):
+    self.auth_token = auth_token
+    self.app_path = app_path
+    self.port = port
+
+  def deploy(self):
+      ngrok.set_auth_token(self.auth_token)
+      def run_streamlit():
+        os.system('streamlit run ' + self.app_path + ' --server.port ' + str(self.port))
+      thread = Thread(target=run_streamlit)
+      thread.start()
+      public_url = ngrok.connect(addr=str(self.port), proto='http', bind_tls=True)
+      return public_url
+
+  def undeploy(self):
+      ngrok.kill()
